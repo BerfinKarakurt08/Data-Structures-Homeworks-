b506787b55276b3dc6885922ba0484d7809acf34
BerfinKarakurt08 <berfinkarakurt86@gmail.com>
2025-12-25 12:48:54 +0300

    Update 3.Odev.c

--- 3.Odev.c
+++ 3.Odev.c
@@ -1,17 +1,64 @@
 #include <stdio.h>
+#include <ctype.h>
+char stack[100];
+int top = -1;
 
-void hanoi(int n, char source, char dest, char aux) {
-    if (n == 1) {
-        printf("Disk 1 tasindi: %c -> %c\n", source, dest);
-        return;
+void push(char c) {
+    stack[++top] = c;
+}
+
+char pop() {
+    if(top == -1)return -1;
+    return stack[top--];
+}
+
+
+int precedence(char c) {
+    if(c=='^') return 3;
+    if(c=='*' || c=='/') return 2;
+    if(c=='+' || c=='-') return 1;
+    return 0;}
+
+void infixToPostfix(char* exp) {
+    int i = 0;
+    while(exp[i] != '\0') {
+        char c = exp[i];
+        
+       
+        if(isalnum(c)) {
+            printf("%c", c);
+        }
+      
+        else if(c == '(') {
+            push(c);
+        }
+       
+        else if(c == ')') {
+            while(top != -1 && stack[top] != '(')
+                printf("%c", pop());
+            pop(); 
+        }
+      
+        else {
+            while(top != -1 && precedence(stack[top]) >= precedence(c))
+                printf("%c", pop());
+            push(c);  }
+        i++;
     }
     
-    hanoi(n - 1, source, aux, dest);
-    printf("Disk %d tasindi: %c -> %c\n", n, source, dest);
-    hanoi(n - 1, aux, dest, source);
+  
+    while(top != -1)
+        printf("%c", pop());
+    
+    printf("\n");
 }
 
 int main() {
-    hanoi(3, 'A', 'C', 'B');
+    char expression[] = "A+B*(C^D-E)";
+    printf("Infix: %s\n", expression);
+    
+    printf("Postfix:");
+    infixToPostfix(expression);
+    
     return 0;
 }
