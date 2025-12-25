a8ab2b4572ba7673f00af1075219078a8c09ff5d
BerfinKarakurt08 <berfinkarakurt86@gmail.com>
2025-12-25 12:53:50 +0300

    Update 4.Odev.c

--- 4.Odev.c
+++ 4.Odev.c
@@ -1,26 +1,63 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-struct Node {
-    int data;
-    struct Node* next;
-    struct Node* prev;
-};
-
-
-void printList(struct Node* head) {
-    struct Node* temp = head;
-    while (temp!=NULL) {
-        printf("%d ", temp->data);
-        temp=temp->next;
-    }
-    printf("\n");
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-void printRecursive(struct Node* head) {
-    if (head == NULL) {
-        return;
+void maxHeapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+   if (right < n && arr[right] > arr[largest])
+        largest = right;
+  if (largest != i) {
+        swap(&arr[i], &arr[largest]);
+        maxHeapify(arr, n, largest);
     }
-    printf("%d ",head->data);
-    printRecursive(head->next);
+}
+
+
+void minHeapify(int arr[], int n, int i) {
+    int smallest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] < arr[smallest])
+        smallest = left;
+  if (right < n && arr[right] < arr[smallest])
+        smallest = right;
+  if (smallest != i) {
+        swap(&arr[i], &arr[smallest]);
+        minHeapify(arr, n, smallest);
+    }
+  }
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; ++i)
+        printf("%d ", arr[i]);
+    printf("\n");
+ }
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Original Array: ");
+    printArray(arr, n);
+
+   for (int i = n / 2 - 1; i >= 0; i--)
+        maxHeapify(arr, n, i);
+    printf("Max Heap: ");
+    printArray(arr, n);
+
+  for (int i = n / 2 - 1; i >= 0; i--)
+        minHeapify(arr, n, i);
+    printf("Min Heap: ");
+    printArray(arr, n);
+
+    return 0;
 }
