95c24f7a6f57e502d9770f430f84acd8e8a7539d
BerfinKarakurt08 <berfinkarakurt86@gmail.com>
2025-12-31 01:08:09 +0300

    Update 10.Odev.c

--- 10.Odev.c
+++ 10.Odev.c
@@ -1,64 +1,43 @@
 #include <stdio.h>
 
-int heap[100];
-int n=0;
+int tree[100];
 
-void swap(int*a, int*b) {
-    int temp = *a;
-    *a=*b;
-    *b=temp;
-
+void setRoot(int key) {
+    tree[0] = key;
 }
-
-void heapify(int i) {
-    int largest =i;
-    int left =2*i + 1;
-    int right =2*i + 2;
-
-    if (left<n && heap[left]>heap[largest])
-        largest = left;
-
-    if (right<n && heap[right] >heap[largest])
-        largest = right;
-
-    if (largest!=i) {
-        swap(&heap[i],&heap[largest]);
-        heapify(largest);
+void setLeft(int key, int parentIndex) {
+    if(tree[parentIndex] == -1) {
+     return;
      }
+    tree[(parentIndex * 2) + 1] = key;
 }
 
-void insert(int val) {
-    heap[n] = val;
-    n++;
-    int i=n-1;
-
-
-    while (i!=0 && heap[(i-1)/2] < heap[i]) {
-        swap(&heap[i], &heap[(i-1)/2]);
-        i =(i-1)/2;
-      }
-   }
-
-void deleteMax() {
-    if(n<=0) return;
-
-    heap[0]=heap[n-1];
-    n--;
-    heapify(0);
+void setRight(int key, int parentIndex) {
+    if(tree[parentIndex] == -1) {
+        return;
+    }
+    tree[(parentIndex * 2) + 2] = key;
  }
 
+void printTree() {
+    for(int i = 0; i < 10; i++) {
+        if(tree[i] != -1)
+         printf("Index %d: %d\n", i, tree[i]);
+        else
+          printf("Index %d: -\n", i);
+       }
+   }
+
 int main() {
-    insert(10);
-    insert(20);
-    insert(5);
-    insert(30);
+    for(int i = 0; i < 100; i++) tree[i] = -1;
 
-    printf("Heap:");
-    for(int i=0;i<n;i++) printf("%d ",heap[i]);
-    printf("\n");
+    setRoot(10);
+    setLeft(20, 0);
+    setRight(30, 0);
+    setLeft(40, 1);
+    setRight(50, 1);
+    setLeft(60, 2);
+    printTree();
 
-    deleteMax();
-    printf("Son durum: ");
-    for(int i=0;i<n;i++) printf("%d ",heap[i]);
-     return 0;
+return 0;
 }
