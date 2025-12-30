33ef1f8e10cf3b08d5c710bc5cf55d82f6df59be
BerfinKarakurt08 <berfinkarakurt86@gmail.com>
2025-12-31 00:51:19 +0300

    Update 7.Odev.c

--- 7.Odev.c
+++ 7.Odev.c
@@ -1,44 +1,63 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#define CAPACITY 100 
 
-struct Node {
-    char data[10];
-    struct Node *left,*right;
-};
+int array[CAPACITY];
+int size = 0; 
 
-struct Node* createNode(char* val) {
-    struct Node* node =(struct Node*)malloc(sizeof(struct Node));
-    strcpy(node->data,val);
-    node->left=NULL;
-    node->right=NULL;
-    return node;
+void printArray() {
+    printf("Dizi: ");
+    for(int i = 0; i < size; i++) {
+         printf("%d ", array[i]);
+    }
+    printf("\n");
 }
 
-void printTree(struct Node* root, int space) {
-    if (root ==NULL) return;
+void insert(int index, int value) {
+    if (size >= CAPACITY) {
+        printf("Dizi dolu, ekleme yapilamaz.\n");
+     return;
+    }
+    if (index < 0 || index > size) {
+        printf("Gecersiz indeks.\n");
+     return;
+    }
+
+  
+    for (int i = size; i > index; i--) {
+        array[i] = array[i-1];
+    }
+      array[index] = value;
+     size++; 
+}
+
+
+void deleteElement(int index) {
+    if (index < 0 || index >= size) {
+        printf("Gecersiz indeks, silme yapilamaz.\n");
+        return;
+    }
+
+     for (int i = index; i < size - 1; i++) {
+        array[i] = array[i+1];
+    }
     
-    space +=5;
-    printTree(root->right,space);
-    
-    printf("\n");
-    for (int i=5; i < space; i++) printf(" ");
-    printf("%s\n",root->data);
-    
-    printTree(root->left, space);
+    size--; 
 }
 
 int main() {
-    struct Node*root=createNode("+");
-    root->left=createNode("-");
-    root->right=createNode("*");
+   
+    insert(0, 10);
+    insert(1, 20);
+    insert(2, 30);
+    insert(3, 40);
+    printArray(); 
     
-    root->left->left=createNode("a");
-    root->left->right=createNode("b");
+    printf("\nIndex 2'ye 25 ekleniyor.\n");
+    insert(2, 25);
+    printArray(); 
     
-    root->right->left=createNode("c");
-    root->right->right=createNode("2");
-
-    printTree(root,0);
-    return 0;
+     printf("\nIndex 1 siliniyor.\n");
+     deleteElement(1);
+     printArray();
+     return 0;
 }
